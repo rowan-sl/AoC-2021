4bf7cbe4f1db2124c560dfe3c4962a2263d07715
Rowan S-L <rowan.with.cats@gmail.com>
2021-12-02 07:04:57 -0500

    first part

---
+++ day-2/sub-movement-1.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+with open("input/in1.txt", "r") as f:
+    lines = f.readlines()
+
+inputs = []
+for l in lines:
+    parts = l.strip().split(" ")
+    inputs.append(parts)
+
+horizontal = 0
+depth = 0
+
+for act in inputs:
+    amnt = int(act[1])
+    match act[0]:
+        case "forward":
+            horizontal += amnt
+        case "up":
+            depth -= amnt
+        case "down":
+            depth += amnt
+    print(act[0], amnt, horizontal, depth)
+
+print("done")
+print(horizontal, depth)
+print("answer")
+print(horizontal*depth)
