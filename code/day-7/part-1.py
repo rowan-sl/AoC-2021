96b3915b4b8f8f918515421f41f398fb755c6e02
Rowan S-L <rowan.with.cats@gmail.com>
2021-12-07 06:02:39 -0500

    day 7 done

---
+++ code/day-7/part-1.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from tqdm import tqdm, trange
+
+with open("input/real.txt") as f:
+    txt = f.read()
+    sints = txt.split(",")
+    ints = [int(i) for i in sints]
+
+def get_fuel(pos, newpos):
+    dist = abs(newpos-pos)
+    used = 0
+    cost = 1
+    for i in range(dist):
+        used += cost
+        cost += 1
+    return used
+
+def get_all_fuel(crabpos, newpos):
+    total = 0
+    for crab in crabpos:
+        total += get_fuel(crab, newpos)
+    return total
+
+all = []
+for i in trange(max(ints)+10):
+    all.append(get_all_fuel(ints, i))
+
+print(min(all))
