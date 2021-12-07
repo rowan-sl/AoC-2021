0efcec7e25f6b3cd2b493747836c1389d355eccb
Rowan S-L <rowan.with.cats@gmail.com>
2021-12-07 06:09:25 -0500

    now works for part 1 and 2

--- code/day-7/part-1.py
+++ code/day-7/part-1.py
@@ -5,8 +5,11 @@ with open("input/real.txt") as f:
     sints = txt.split(",")
     ints = [int(i) for i in sints]
 
-def get_fuel(pos, newpos):
+def get_fuel(pos, newpos, part):
     dist = abs(newpos-pos)
+    #for part 1
+    if part == 1:
+        return dist
     used = 0
     cost = 1
     for i in range(dist):
@@ -14,14 +17,18 @@ def get_fuel(pos, newpos):
         cost += 1
     return used
 
-def get_all_fuel(crabpos, newpos):
+def get_all_fuel(crabpos, newpos, part):
     total = 0
     for crab in crabpos:
-        total += get_fuel(crab, newpos)
+        total += get_fuel(crab, newpos, part)
     return total
 
-all = []
-for i in trange(max(ints)+10):
-    all.append(get_all_fuel(ints, i))
+def get_answers(part):
+    answers = []
+    for i in trange(max(ints)+10):
+        answers.append(get_all_fuel(ints, i, part))
+    return min(answers)
 
-print(min(all))
+print(get_answers(1))
+print(get_answers(2))
+
